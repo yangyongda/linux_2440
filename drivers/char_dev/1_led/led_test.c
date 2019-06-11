9e820fff4437b54ffa09bf5584e8295d9f673a52
yangyongda <546935748@qq.com>
2019-06-11 16:06:08 +0800

    add led test app

---
+++ drivers/char_dev/1_led/led_test.c
@@ -0,0 +1,37 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/* led_test on
+  * led_test off
+  */
+int main(int argc, char **argv)
+{
+	int fd;   //文件句柄
+	int val = 1;
+	fd = open("/dev/xyz", O_RDWR);  //使用读写模式打开/dev/xyz设备文件
+	if (fd < 0)
+	{
+		printf("can't open!\n");  //打开失败
+	}
+	if (argc != 2)   //参数个数不为2，则显示使用方法
+	{
+		printf("Usage :\n");
+		printf("%s <on|off>\n", argv[0]);
+		return 0;
+	}
+
+	if (strcmp(argv[1], "on") == 0)   //第二个参数为"on"
+	{
+		val  = 1;
+	}
+	else
+	{
+		val = 0;
+	}
+	
+	write(fd, &val, 4);   //写入4个字节数据到设备文件,该函数最终将调用底层驱动的write函数
+	return 0;
+}
--- root_fs/README.md
+++ root_fs/README.md
@@ -8,5 +8,5 @@ mkyaffs2image  fs_2440/  fs_2440.yaffs
 
 注意:
 
-要在fs_2440文件夹创建空文件夹dev mnt temp sys等, 并且要在dev文件夹下创建一些设备节点。
+要在fs_2440文件夹创建空文件夹dev mnt temp sys proc等, 并且要在dev文件夹下创建一些设备节点。
 具体见DOC目录。否则会出问题
